e63cd92bbf39a60582e955a39a5e37ee9996f261
umair <eeumairali@gmail.com>
2025-04-15 13:46:44 +0500

    code corrected

--- Pearson_Book/Chapter 13 GUI/part 4 grids.py
+++ Pearson_Book/Chapter 13 GUI/part 4 grids.py
@@ -12,13 +12,13 @@ keypad = tk.Frame()
 keypad.pack(side='bottom')
 
 
-b1 = tk.Button(s=keypad,text='1')
+b1 = tk.Button(keypad,text='1')
 b1.grid(row=0,column = 0)
-b2 = tk.Button(s=keypad,text='2')
+b2 = tk.Button(keypad,text='2')
 b2.grid(row=0,column = 1)
-b3 = tk.Button(s=keypad,text='3')
+b3 = tk.Button(keypad,text='3')
 b3.grid(row=0,column = 2)
-b4 = tk.Button(s=keypad,text='4')
+b4 = tk.Button(keypad,text='4')
 b4.grid(row=0,column = 3)
 
 
